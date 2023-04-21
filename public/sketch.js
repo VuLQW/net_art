df0c415d501622c3039c2124149188c6749c2427
Vu <vule.47392@gmail.com>
2023-04-21 17:38:59 +1000

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,13 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+const cnv = document.getElementById (`NTcanvas`)
+cnv.style.background = ""
+
 function setup () {
    createCanvas (innerWidth, innerHeight)
 }
 
 function  draw () {
-   background (`turquoise`)
+   background (`black`)
 }

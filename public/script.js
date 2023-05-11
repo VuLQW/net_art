468ed33b5717c9055b4a0b3f4d2516ca5f1abf4e
Vu <vule.47392@gmail.com>
2023-05-12 02:39:17 +1000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,70 @@
-document.body.style.margin   = 0
-document.body.style.overflow = `hidden`
+//  document.body.style.margin   = 0
+//  document.body.style.overflow = `hidden`
 
-const cnv = document.getElementById("cnv_id")
-cnv.width = innerWidth
-cnv.height = innerHeight
-cnv.style.background = "turquoise"
+// const cnv = document.getElementById("cnv_id")
+// cnv.width = innerWidth
+// cnv.height = innerHeight
+// cnv.style.background = "turquoise"
 
-<iframe src="https://editor.p5js.org/VuLQW/full/If-LpTGB8"></iframe>
+//  const ctx = cnv.getContext (`2d`)
 
-function resize_canvas () {
-   cnv.width  = innerWidth
-   cnv.height = innerHeight   
-}
+//  const squuares = []
+
+//  const mouse_pos = new Vector (0,0)
+
+//  //console.dir
+//  cnv.onpointerover = e => {
+//    mouse_pos.x = e.x
+//    mouse_pos.y = e.y
+//  }
+
+
+
+// cnv.onpointerdown = (e) => {
+//    squuares.push (new Vector (e.x, e.y))
+   // console.log (`interaction achieved!!`)
+   // console.dir(e)
+   // console.log(e.x,e.y)
+   // ctx.fillStyle = "white"
+   // ctx.fillRect (e.x-50,e.y-50,100,100 )
+   //console.log (e.x, e.y)
+   //squuares.push ({
+   // x: e.x,
+   // y: e.y
+   //})
+   
+//}
+
+
+// function draw_frame () {
+//    ctx.fillStyle = `turquoise`
+//    ctx.fillRect (0,0,cnv.width, cnv.height)
+
+//    squuares.forEach (s => {
+
+//       const movement = new Vector (s.x, s.y)
+//       movement.subtract (mouse_pos)
+//       movement.setMag (3)
+//       s.add (movement)
+//       // s.x += Math.random () * 10 - 3
+//       // s.y += Math.random () * 10 - 3
+//       ctx.fillStyle = `deeppink`
+//       ctx.fillRect (s.x - 10, s.y - 10, 20, 20)
+
+//    })
+
+
+
+//    requestAnimationFrame (draw_frame)
+   
+
+   
+// }
+
+// requestAnimationFrame (draw_frame)
+
+// function resize_canvas () {
+//    cnv.width  = innerWidth
+//    cnv.height = innerHeight   
+// }
 
